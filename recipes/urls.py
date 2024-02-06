c1bcf5495cef0d3568f37b3da0f87113e171a17d
Thales Radicchi <thalesradicchi@gmail.com>
2024-02-06 14:58:47 -0300

    Adiciona e separa os arquivos para que haja aplicação com herança e alteração da número das páginas, para que seja feito de acordo com o conteúdo

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+# recipes:recipe
+app_name = "recipes"
+
 urlpatterns = [
-    path("", views.home), #Home
-    path("recipes/<int:id>/", views.recipe),
+    path("", views.home, name="home"), #Home
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
